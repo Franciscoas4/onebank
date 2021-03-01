539f613637b1e21c403474377ba7c73b00a331f1
Franciscoas4 <franciscoas4@gmail.com>
2021-03-01 19:36:49 -0300

    create account class

---
+++ src/main/kotlin/com/aula/kotlin/Conta.kt
@@ -0,0 +1,17 @@
+package com.aula.kotlin
+
+import java.math.BigDecimal
+
+class Conta (
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+) {
+    fun depositar(valor: BigDecimal) {
+
+    }
+
+    fun saque(valor: BigDecimal) {
+
+    }
+}
