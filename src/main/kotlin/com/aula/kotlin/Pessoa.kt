67dbb8951b2a8614ba8d6de0bb085cb0ecb91732
Franciscoas4 <franciscoas4@gmail.com>
2021-03-01 18:56:01 -0300

    Update to apply the private modifier to set function of cpf propertir

--- src/main/kotlin/com/aula/kotlin/Pessoa.kt
+++ src/main/kotlin/com/aula/kotlin/Pessoa.kt
@@ -3,11 +3,13 @@ package com.aula.kotlin
 class Pessoa {
     var nome: String = "Francisco"
     var cpf: String = "123.456.789-00"
+    private set
 }
 
 fun main() {
     val francisco = Pessoa()
 
+    println(francisco)
     println(francisco.nome)
     println(francisco.cpf)
 }
