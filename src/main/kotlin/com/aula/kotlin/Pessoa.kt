b02dc78a77faba0e7a42d6da91baa4087031922a
Franciscoas4 <franciscoas4@gmail.com>
2021-03-01 21:04:23 -0300

    updated to make person and employer abstract class

--- src/main/kotlin/com/aula/kotlin/Pessoa.kt
+++ src/main/kotlin/com/aula/kotlin/Pessoa.kt
@@ -1,16 +1,6 @@
 package com.aula.kotlin
 
-class Pessoa {
-    var nome: String = "Francisco"
-    var cpf: String = "123.456.789-00"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome : $cpf"
-}
-
-fun main() {
-    val francisco = Pessoa()
-    println(francisco.pessoaInfo())
-}
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+)
